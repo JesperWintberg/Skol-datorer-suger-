7847678ea2a470528c9e1cabff0e9b912a36f230
JesperWintberg <111962418+JesperWintberg@users.noreply.github.com>
2022-11-20 17:52:58 +0100

    två nya random variabler, gjort random till alla 4 delar i kortleken

--- Black Jack/Program.cs
+++ Black Jack/Program.cs
@@ -12,19 +12,29 @@ namespace ____
             int[] diamonds = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
             int[] spades = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
 
+            //skapar random variabel 
+            Random kort = new Random();
+            Random kort2 = new Random();
+            Random Kort3 = new Random();
+
+            //skapar en ny int variabel som är ramdom av delarna i kortleken
+            int rndmheart = hearts[kort.Next(0, 11)];
+            int rndmclubs = clubs[kort.Next(0, 11)];
+            int rndmdimonds = diamonds[kort.Next(0, 11)];
+            int rndmspades = spades[kort.Next(0, 11)];
+
+            
+
             //Start av spel 
             Console.WriteLine("Hej och välkommen till detta Black jack spel");
             Console.WriteLine("Om du vill börja spelet skriv in ett Y om du vill avsluta programet skriv ett N.");
             string start =Console.ReadLine();
             start.ToLower();
 
-            //skapar random variabel 
-            Random kort = new Random();
-
 
             //Start frågor innnan spelet börjar          
-             for (int i = 0; i <1; i++)
-             {
+            for (int i = 0; i <1; i++)
+            {
                 if (start == "y")
                 {
                     Console.WriteLine("Startar");
@@ -41,8 +51,12 @@ namespace ____
                     Console.WriteLine("Error");
                     break;
                 }
-             }
+            }
 
+            for(int k = 0; k <1; k++)
+            {
+                
+            }
         }
     }
 }
